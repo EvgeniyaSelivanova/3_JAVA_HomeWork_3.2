b5b448f5fc7c869c6d74840b9e35235ac865c7c4
Evgeniya Selivanova <kulbatskaya.evgenia@gmail.com>
2020-12-22 16:58:57 +0300

    Final Version.2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        float replenishment = 1100.5F;
+        float replenishment = 100.5F;
         float balance = 100.75F;
         if (replenishment>1000) {
-            float bonus=replenishment/100;
-            int final_bonus=(int) bonus;
-            float final_balance=balance+replenishment+final_bonus;
-            System.out.println(final_balance);
+            int bonus=(int) replenishment/100;
+            balance=balance+replenishment+bonus;
         }
         else {
-            float final_balance=balance+replenishment;
-            System.out.println(final_balance);
-        }
+            balance=balance+replenishment;
+             }
+        System.out.println(balance);
     }
 }
